b01ed6ca4de563e170e8213aa998c4dfdfe6d8e9
skari666 <144269531+skari666@users.noreply.github.com>
2023-09-21 20:22:11 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Введите координаты первой точки
+        Console.WriteLine("Введите координаты первой точки:");
+        Console.Write("x1: ");
+        double x1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("y1: ");
+        double y1 = Convert.ToDouble(Console.ReadLine());
+
+        // Введите координаты второй точки
+        Console.WriteLine("Введите координаты второй точки:");
+        Console.Write("x2: ");
+        double x2 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("y2: ");
+        double y2 = Convert.ToDouble(Console.ReadLine());
+
+        // Вычисление коэффициента 
+        double k = (y1 - y2) / (x1 - x2);
+
+        // Вычисление b
+        double b = y2 - k * x2;
+
+        // Вывод уравнения прямой
+        Console.WriteLine($"Уравнение прямой: y = {k:F2}x + {b:F2}");
+        Console.ReadKey();
+    }
+}
